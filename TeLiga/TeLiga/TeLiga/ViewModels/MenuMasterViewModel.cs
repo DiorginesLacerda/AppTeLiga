c45abdbf08ab226b5a0a077bd0628ab9a1bb3437
Diorgines Lacerda <diorgines.lacerda@hotmail.com>
2017-11-07 19:06:08 -0200

    Formated a NewAccountEditView

--- TeLiga/TeLiga/TeLiga/ViewModels/MenuMasterViewModel.cs
+++ TeLiga/TeLiga/TeLiga/ViewModels/MenuMasterViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace TeLiga.ViewModels
 {
@@ -20,7 +21,12 @@ namespace TeLiga.ViewModels
                 {
                     MessagingCenter.Send(this, "CreateNewEvent");
                 });
-            this.ExitCommand = new Command(() => { });
+            this.ExitCommand = new Command(
+                () => 
+                {
+                    
+                });
+            
         }
 
     }
