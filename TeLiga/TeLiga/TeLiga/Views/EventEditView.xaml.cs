755053293fe581a6ec6cb3b62c54af53dcb3ddf7
Diorgines Lacerda <diorgines.lacerda@hotmail.com>
2017-11-07 15:19:12 -0200

    Created a Simple View: EventEditView

--- TeLiga/TeLiga/TeLiga/Views/EventEditView.xaml.cs
+++ TeLiga/TeLiga/TeLiga/Views/EventEditView.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TeLiga.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,9 +11,12 @@ namespace TeLiga.Views
 {
     public partial class EventEditView : ContentPage
     {
+        EventEditViewModel ViewModel;
         public EventEditView()
         {
             InitializeComponent();
+            this.ViewModel = new EventEditViewModel();
+            this.BindingContext = this.ViewModel;
         }
     }
 }
